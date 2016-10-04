c6e1536991951eb4d4ab1343984f7ef7db6496c7
Henry Nguyen <mocchimon@users.noreply.github.com>
2016-10-03 22:49:08 -0700

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,11 @@
+// my first Java program 
+public class Lab0  
+{ 
+    public static void main (String[] args) 
+    { 
+        System.out.println("Hello and Welcome to CPSC1150!"); 
+    	System.out.println("My Name is Henry Nguyen");  
+	System.out.println("My Student Number is 100195039");  
+	System.out.println("My Email Address is henrynguyen123@hotmail.com"); 
+	} 
+}
